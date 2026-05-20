1b682a0c1382df6014f0741f243966a1552cae61
aleciajsocial <aleciajsocial@gmail.com>
2026-05-20 19:05:43 -0400

    fix: simplify db index to fix query errors

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle }   from 'drizzle-orm/postgres-js';
 import postgres       from 'postgres';
 import * as schema    from './schema';
-import * as relations from './relations';
 
 const globalForDb = globalThis as unknown as { conn: postgres.Sql | undefined };
+
 const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 if (process.env.NODE_ENV !== 'production') globalForDb.conn = conn;
 
-export const db = drizzle(conn, { schema: { ...schema, ...relations } });
+export const db = drizzle(conn, { schema });
